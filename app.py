32b74181eecd5e9c73d71653045d8370341c58e6
Talha Yousaf Saeed <109076602+SSTalha@users.noreply.github.com>
2023-08-05 11:10:15 +0000

    commit this

--- app.py
+++ app.py
@@ -2,12 +2,14 @@ import streamlit as st
 from dotenv import load_dotenv
 from PyPDF2 import PdfReader
 from langchain.text_splitter import CharacterTextSplitter
-# from langchain.embeddings import OpenAIEmbeddings  # If using openai embedding import this
 from langchain.embeddings import HuggingFaceInstructEmbeddings
 from langchain.vectorstores import FAISS
-from langchain.chat_models import ChatOpenAI
 from langchain.memory import ConversationBufferMemory
 from langchain.chains import ConversationalRetrievalChain
+from langchain.llms import HuggingFaceHub
+from htmlTemplates import css,bot_template,user_template
+# from langchain.embeddings import OpenAIEmbeddings  # If using openai embedding import this
+# from langchain.chat_models import ChatOpenAI
 
 
 
@@ -35,14 +37,17 @@ def get_text_chunks(text):
 
 def get_vector_store(text_chunks):
     # embeddings = OpenAIEmbeddings()
-    embeddings = HuggingFaceInstructEmbeddings(model_name = "hkunlp/instructor-xl")
+    embeddings = HuggingFaceInstructEmbeddings(model_name = "sentence-transformers/clip-ViT-L-14")
     vectorstore = FAISS.from_texts(texts=text_chunks, embedding=embeddings)
     return vectorstore
 
 #--------------------------------------------
-#Done using Langchain.chat_models (Chat OpenAI)
+
 def get_conversation_chain(vector_store):
-    llms = ChatOpenAI()
+    # Uncomment the ChatOpenAI lib to use the OpenAI llm
+    # llms = ChatOpenAI()
+    llms = HuggingFaceHub(repo_id="Kaludi/chatgpt-gpt4-prompts-bart-large-cnn-samsum", model_kwargs={"temperature":0.5, "max_length":512})
+
     memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
     conversation_chain = ConversationalRetrievalChain.from_llm(
         llm= llms,
@@ -51,19 +56,39 @@ def get_conversation_chain(vector_store):
     )
     return conversation_chain
 
+#--------------------------------------------
 
+def handle_userinput(user_question):
+    response = st.session_state.conversation({'question': user_question})
+    st.session_state.chat_history = response['chat_history']
+
+    for i, message in enumerate(st.session_state.chat_history):
+        if i % 2 == 0:
+            st.write(user_template.replace(
+                "{{MSG}}", message.content), unsafe_allow_html=True)
+        else:
+            st.write(bot_template.replace(
+                "{{MSG}}", message.content), unsafe_allow_html=True)
 
 #--------------------------------------------
 
+
+
 def main():
     load_dotenv()
-    st.set_page_config(page_title="Chat with PDFs", page_icon=":books:")
+    st.set_page_config(page_title="Chat with multiple PDFs",
+                       page_icon=":books:")
+    st.write(css, unsafe_allow_html=True)
 
     if "conversation" not in st.session_state:
         st.session_state.conversation = None
+    if "chat_history" not in st.session_state:
+        st.session_state.chat_history = None
 
-    st.header("Chat with PDFs :books:")
-    st.text_input("Ask a question related to your documents:")
+    st.header("Chat with multiple PDFs :books:")
+    user_question = st.text_input("Ask a question about your documents:")
+    if user_question:
+        handle_userinput(user_question)
 
     with st.sidebar:
         st.subheader("Your documents")
@@ -74,18 +99,16 @@ def main():
                 # get pdf text
                 raw_text = get_pdf_text(pdf_docs)
 
-                
-                # convert text to chunks
+                # get the text chunks
                 text_chunks = get_text_chunks(raw_text)
-                st.write(text_chunks)
 
                 # create vector store
-                vector_store = get_vector_store(text_chunks)
+                vectorstore = get_vector_store(text_chunks)
 
                 # create conversation chain
-                st.session_state.conversation = get_conversation_chain(vector_store)
+                st.session_state.conversation = get_conversation_chain(
+                    vectorstore)
 
 
-
-if __name__  == '__main__':
+if __name__ == '__main__':
     main()
